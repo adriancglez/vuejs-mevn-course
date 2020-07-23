1e3b3157c03f64ec5fb7e798ac5250439f4f8f3e
Adrián González <yo.robot.1990@gmail.com>
2020-07-23 12:45:41 -0500

    Se integra el tema Templates String de Javascript - reolve #7

---
+++ 01_Fundamentos_JavaScript/03_Templates_String.js
@@ -0,0 +1,11 @@
+// Concatenando con el operador +
+const numero = (num) => {
+    return 'El número es: ' + num;
+}
+
+// De forma simplificada:
+const numero = (numero1m, numero2) => (`La suma es: ${numero2 + numero2}`);
+
+const resultado = numero(10);
+
+console.log(resultado);
