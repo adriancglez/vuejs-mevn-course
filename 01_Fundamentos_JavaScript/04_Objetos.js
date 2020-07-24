97a7df6c9a5f7aaa3273a9de4dff4effe75a68fa
Adrián González <yo.robot.1990@gmail.com>
2020-07-24 09:17:25 -0500

    Se integra tema destructuring objects en JavaScript - resolve #11

--- 01_Fundamentos_JavaScript/04_Objetos.js
+++ 01_Fundamentos_JavaScript/04_Objetos.js
@@ -1,5 +1,4 @@
 // Objetos
-
 const mascota = {
     nombre: 'Tom',
     edad: 10,
@@ -14,3 +13,11 @@ console.log(mascota.nombre);
 mascota.id = 1;
 
 console.log(mascota);
+
+console.log(mascota.nombre);
+
+
+// Destructuring Objects
+const { nombre, vivo, edad } = mascota;
+
+console.log(edad);
