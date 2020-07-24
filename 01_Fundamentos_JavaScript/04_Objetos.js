cc05663bb24525e07c7bd46b269aa8aac3e3d6fb
Adrián González <yo.robot.1990@gmail.com>
2020-07-24 09:04:40 -0500

    Se integra tema de objetos en Javascript - resolve #9

---
+++ 01_Fundamentos_JavaScript/04_Objetos.js
@@ -0,0 +1,16 @@
+// Objetos
+
+const mascota = {
+    nombre: 'Tom',
+    edad: 10,
+    vivo: true,
+    razas: ['peludo', 'negro']
+}
+
+console.log(mascota);
+
+console.log(mascota.nombre);
+
+mascota.id = 1;
+
+console.log(mascota);
