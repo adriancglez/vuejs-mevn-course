0f3c8ceacac3b7d327a0d4d4fe8e5858394bc854
Adrián González <yo.robot.1990@gmail.com>
2020-07-24 09:41:18 -0500

    Se integran nuevos ejercicios de objetos en JavaScript - resolve #13

--- 01_Fundamentos_JavaScript/04_Objetos.js
+++ 01_Fundamentos_JavaScript/04_Objetos.js
@@ -21,3 +21,30 @@ console.log(mascota.nombre);
 const { nombre, vivo, edad } = mascota;
 
 console.log(edad);
+
+// Practicando Objetos
+const web = {
+    nombre: 'andromeda',
+    links: {
+        enlace: 'www.andromeda.com'
+    },
+    redesSociales: {
+        youtube: {
+            enlace: 'youtube.com/andromexico',
+            nombre: 'andromeda yt'
+        },
+        facebook: {
+            enlace: 'facebook.com/andromexico',
+            nombre: 'andromeda fb'
+        }
+    }
+}
+
+// Si se desea acceder a la propiedad enlace del objeto Youtube dentro del objeto web
+const enlaceYT = web.redesSociales.youtube.enlace;
+console.log(enlaceYT);
+
+// De la forma con destructuring objects
+const { enlace, nombre } = web.redesSociales.youtube;
+console.log(enlace);
+console.log(nombre);
